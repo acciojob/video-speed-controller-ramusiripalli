a5e6af67f0662e303d9268120cd97a526c18c575
ramu siripalli <ramsiripalli2772@gmail.com>
2026-01-10 16:12:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('video');
+const speed = document.querySelector('.speed');
+const speedBar = document.querySelector('.speed-bar');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+const min = 0.5;
+const max = 4;
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function handleMove(e) {
+  const y = e.offsetY;
+  const percent = y / speed.offsetHeight;
+
+  const playbackRate = percent * (max - min) + min;
+
+  const height = Math.round(percent * 100) + '%';
+
+  speedBar.style.height = height;
+  speedBar.textContent = playbackRate.toFixed(2) + '×';
+  video.playbackRate = playbackRate;
+}
+
+speed.addEventListener('mousemove', handleMove);
